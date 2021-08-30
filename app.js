f42a12cb7becfc0ac3464510c425a46c88419922
andreadipalma <andrea.dipalma@gmail.com>
2021-08-30 22:35:21 +0200

    logger

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  logger.log(err.message);
 
   // render the error page
   res.status(err.status || 500);
