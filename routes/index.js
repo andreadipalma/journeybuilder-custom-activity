2f4ec6b26acf65e6ac8fe9afac9b4dee46307248
andreadipalma <andrea.dipalma@gmail.com>
2021-08-30 22:54:27 +0200

    error page

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,6 @@ exports.config = (req, res) => {
 
   const configTemplate = fs.readFileSync(file, 'utf-8');
   const config = JSON.parse(configTemplate.replace(/\$DOMAIN/g, domain));
-  logger.log("DEBUG::: " + config);
   res.json(config);
 };
 
