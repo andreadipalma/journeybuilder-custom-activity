60b6eb3adc07a4e6aa12862e128343d7accd1d2a
andreadipalma <andrea.dipalma@gmail.com>
2021-08-30 22:46:07 +0200

    Fix 1

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  logger1.log(err.message);
+  logger1.log(JSON.stringify(err.message));
 
   // render the error page
   res.status(err.status || 500);
